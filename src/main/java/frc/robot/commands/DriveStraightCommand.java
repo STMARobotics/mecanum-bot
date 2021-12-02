db4d2afa37a7f00d05a0df3fb0b94450899bdeaf
Brandon Gasser <34321725+brandongasser@users.noreply.github.com>
2021-12-01 19:35:14 -0600

    added DriveStraightCommand and SpinCommand

---
+++ src/main/java/frc/robot/commands/DriveStraightCommand.java
@@ -0,0 +1,42 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.subsystems.DrivetrainSubsystem;
+
+public class DriveStraightCommand extends CommandBase {
+
+    private final DrivetrainSubsystem drivetrainSubsystem;
+    private final Timer timer = new Timer();
+    private final double power;
+    private final int seconds;
+
+    public DriveStraightCommand(DrivetrainSubsystem drivetrainSubsystem, double power, int seconds) {
+        this.drivetrainSubsystem = drivetrainSubsystem;
+        this.power = power;
+        this.seconds = seconds;
+        addRequirements(drivetrainSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+        timer.start();
+    }
+
+    @Override
+    public void execute() {
+        drivetrainSubsystem.drive(0, power, 0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return timer.get() >= seconds;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        drivetrainSubsystem.drive(0, 0, 0);
+        timer.stop();
+    }
+
+}
